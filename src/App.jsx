b2d1155a482f017c90a838c2ffb701ae174c30dd
memolop10 <inglopezvargas@gmail.com>
2021-03-03 16:06:29 -0600

    inicializando firestore

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,23 @@
+import React from 'react'
+import {firebase} from './firebase'
 
 function App() {
+
+  React.useEffect(() => {
+
+    const getData = async() => {
+      try {
+        const db = firebase.firestore();
+        const data = await db.collection('tareas').get()
+        console.log(data.docs)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    getData();
+  })
+
   return (
     <div className="container">
      Soy el MEMO
